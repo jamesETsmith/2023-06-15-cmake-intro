a8f13b85eae145aa42aa2ad6e0f0af588c862fab
James Smith <james.smith9113@gmail.com>
2023-06-13 21:01:25 -0400

    Updating examples

---
+++ examples/02-simple-app/simple_app.cpp
@@ -0,0 +1,7 @@
+#include "simple_lib.hpp"
+
+int main() {
+  std::cout << "I'm in the app" << std::endl;
+  lib_func();
+  return 0;
+}
---
+++ examples/02-simple-app/simple_lib.cpp
@@ -0,0 +1,3 @@
+#include "simple_lib.hpp"
+
+void lib_func() { std::cout << "I'm a library function" << std::endl; }
